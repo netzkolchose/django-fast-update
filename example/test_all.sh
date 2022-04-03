53320359540f5253f4f8d13e9e968492701f3808
Jörg Breitbart <jerch@rockborn.de>
2022-04-03 19:40:05 +0200

    fix coveralls

--- example/test_all.sh
+++ example/test_all.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
-echo
-echo "TEST: sqlite 3.33"
-DBENGINE=sqlite coverage run --parallel-mode --source='fast_update' ./manage.py test || exit 1
-echo
-echo
-echo "TEST: postgres"
-DBENGINE=postgres  coverage run --parallel-mode --source='fast_update' ./manage.py test || exit 1
-echo
-echo
-echo "TEST: mariadb"
-DBENGINE=mysql coverage run --parallel-mode --source='fast_update' ./manage.py test || exit 1
-echo
-echo
-echo "TEST: mysql8"
-DBENGINE=mysql8 coverage run --parallel-mode --source='fast_update' ./manage.py test || exit 1
-echo
-echo
+echo -e "\nTEST: sqlite 3.33"
+DBENGINE=sqlite coverage run --parallel-mode --source='fast_update' ./example/manage.py test || exit 1
+echo -e "\nTEST: postgres"
+DBENGINE=postgres  coverage run --parallel-mode --source='fast_update' ./example/manage.py test || exit 1
+echo -e "\nTEST: mariadb"
+DBENGINE=mysql coverage run --parallel-mode --source='fast_update' ./example/manage.py test || exit 1
+echo -e "\nTEST: mysql8"
+DBENGINE=mysql8 coverage run --parallel-mode --source='fast_update' ./example/manage.py test || exit 1
 coverage combine
 coverage report
-coverage html
+#coverage html
