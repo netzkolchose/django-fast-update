99c219dca1455b05456bb11eb6e371b2b9203fad
Jörg Breitbart <jerch@rockborn.de>
2022-04-03 19:11:42 +0200

    try matrix on django version

--- example/test_all.sh
+++ example/test_all.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-echo "test sqlite 3.33"
+echo
+echo "TEST: sqlite 3.33"
 DBENGINE=sqlite coverage run --parallel-mode --source='fast_update' ./manage.py test || exit 1
-echo "test postgres"
+echo
+echo
+echo "TEST: postgres"
 DBENGINE=postgres  coverage run --parallel-mode --source='fast_update' ./manage.py test || exit 1
-echo "test mariadb"
+echo
+echo
+echo "TEST: mariadb"
 DBENGINE=mysql coverage run --parallel-mode --source='fast_update' ./manage.py test || exit 1
-echo "test mysql8"
+echo
+echo
+echo "TEST: mysql8"
 DBENGINE=mysql8 coverage run --parallel-mode --source='fast_update' ./manage.py test || exit 1
+echo
+echo
 coverage combine
 coverage report
 coverage html
