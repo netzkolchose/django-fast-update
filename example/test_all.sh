eec7de51b808ca2ed1c49560170c166b8c357084
Jörg Breitbart <jerch@rockborn.de>
2022-04-03 17:57:19 +0200

    make script fail on test errors

--- example/test_all.sh
+++ example/test_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-DBENGINE=sqlite coverage run --parallel-mode --source='fast_update' ./manage.py test
-DBENGINE=postgres  coverage run --parallel-mode --source='fast_update' ./manage.py test
-DBENGINE=mysql coverage run --parallel-mode --source='fast_update' ./manage.py test
-DBENGINE=mysql8 coverage run --parallel-mode --source='fast_update' ./manage.py test
+DBENGINE=sqlite coverage run --parallel-mode --source='fast_update' ./manage.py test || exit 1
+DBENGINE=postgres  coverage run --parallel-mode --source='fast_update' ./manage.py test || exit 1
+DBENGINE=mysql coverage run --parallel-mode --source='fast_update' ./manage.py test || exit 1
+DBENGINE=mysql8 coverage run --parallel-mode --source='fast_update' ./manage.py test || exit 1
 coverage combine
 coverage report
 coverage html
