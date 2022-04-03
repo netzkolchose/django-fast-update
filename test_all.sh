c1ba27936de5283ae1bf17d4fe0b9f747753c51d
Jörg Breitbart <jerch@rockborn.de>
2022-04-03 19:50:15 +0200

    fix coveralls

---
+++ test_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo -e "\nTEST: sqlite 3.33"
+DBENGINE=sqlite coverage run --parallel-mode --source='fast_update' ./example/manage.py test exampleapp || exit 1
+echo -e "\nTEST: postgres"
+DBENGINE=postgres  coverage run --parallel-mode --source='fast_update' ./example/manage.py test exampleapp || exit 1
+echo -e "\nTEST: mariadb"
+DBENGINE=mysql coverage run --parallel-mode --source='fast_update' ./example/manage.py test exampleapp || exit 1
+echo -e "\nTEST: mysql8"
+DBENGINE=mysql8 coverage run --parallel-mode --source='fast_update' ./example/manage.py test exampleapp || exit 1
+coverage combine
+coverage html
+coverage report
