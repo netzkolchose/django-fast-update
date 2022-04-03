dc621bfafac374fd7d2993218a04d1f06db42fad
Jörg Breitbart <jerch@rockborn.de>
2022-04-03 18:58:36 +0200

    output db backend tested

--- example/test_all.sh
+++ example/test_all.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+echo "test sqlite 3.33"
 DBENGINE=sqlite coverage run --parallel-mode --source='fast_update' ./manage.py test || exit 1
+echo "test postgres"
 DBENGINE=postgres  coverage run --parallel-mode --source='fast_update' ./manage.py test || exit 1
+echo "test mariadb"
 DBENGINE=mysql coverage run --parallel-mode --source='fast_update' ./manage.py test || exit 1
+echo "test mysql8"
 DBENGINE=mysql8 coverage run --parallel-mode --source='fast_update' ./manage.py test || exit 1
 coverage combine
 coverage report
