cb76f02df206d41b1ac9b36ff8ec828c47bdd8a8
Jörg Breitbart <jerch@rockborn.de>
2022-04-05 01:01:36 +0200

    fix postgres CI tests

--- test_all.sh
+++ test_all.sh
@@ -4,6 +4,7 @@ echo -e "\nTEST: sqlite 3.33"
 DBENGINE=sqlite coverage run --parallel-mode --source='fast_update' ./example/manage.py test exampleapp || exit 1
 echo -e "\nTEST: postgres"
 DBENGINE=postgres  coverage run --parallel-mode --source='fast_update' ./example/manage.py test exampleapp || exit 1
+DBENGINE=postgres  coverage run --parallel-mode --source='fast_update' ./example/manage.py test postgres_tests || exit 1
 echo -e "\nTEST: mariadb"
 DBENGINE=mysql coverage run --parallel-mode --source='fast_update' ./example/manage.py test exampleapp || exit 1
 echo -e "\nTEST: mysql8"
